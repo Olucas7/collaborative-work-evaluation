ca0e9180137061b1ab1b1182c39dae250fd10058
Oscar Lucas <olucas@espol.edu.ec>
2022-08-28 22:13:36 -0500

    Testing deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WELCOME WORK EVALUATION GROUP
+          WELCOME WORK EVALUATION GROUP ING SOFTWARE 1
         </p>
         <a
           className="App-link"
